13741165a1ec9e277458a7ee4350f975b1f7971c
anandgitacc <anand.akmail@gmail.com>
2023-10-05 10:06:53 +0530

    test1

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,30 @@
+package org.employee;
+
+public class EmployeeDetails { // block or body of class
+	
+	// private -> access specifier
+	// void    -> return type of given method (empty)
+	// empName -> method name
+	// ()      -> arguments place or input or parameter place
+	// {}      -> block or body of the method
+	private void empName() {
+		// implementation detail / business logic
+		// "" -> double quote -> String 
+		System.out.println("Employee name: Anand");
+	}
+	
+	private void empMobile() {
+		System.out.println("Employee mobile: 8939278836");
+	}
+	
+	public static void main(String[] args) {
+		
+		// ClassName objName/refName = new ClassName
+		EmployeeDetails e = new EmployeeDetails();
+		e.empName();
+		e.empMobile();
+		
+	}
+	
+
+}
